5a5de69768e99bb582a5c1b64dac0d171f5c9eca
Mike Warren <mikewarren@Mikes-MacBook-Pro.local>
2026-02-27 08:50:33 -0500

    Added mixpanel

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { PostHogProvider } from '@posthog/react'
+import mixpanel from "mixpanel-browser";
 
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: '2026-01-30',
 } as const
 
+mixpanel.init('1d4970e4b46baeb599bcb29fb17c7947', {
+  autocapture: true,
+  record_sessions_percent: 100,
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
